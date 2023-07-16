66b803ab2317ba41fcafc866bac667a09088e7b2
Rolan <125482806+Debouncced@users.noreply.github.com>
2023-07-16 23:51:47 +0200

    Add files via upload

--- assets/scripts/typeText.js
+++ assets/scripts/typeText.js
@@ -1,1 +1,26 @@
-let description=document.getElementById("description"),idx=0;const descriptionText="Свою первую строчку кода написал ещё 2 года назад, когда узнал что мой приятель умеет создавать сайты и захотелось самому) Тогда и начался мой путь frontend разработчика на react.";function typeWriter(){idx<descriptionText.length&&(description.innerHTML+=descriptionText.charAt(idx),idx++,setTimeout(typeWriter,30))}let options={rootMargin:"0px",threshold:0};target=document.querySelector(".about__code"),observerDescription=new IntersectionObserver((e=>{e[0].isIntersecting&&typeWriter()}),options),observerDescription.observe(target);
+let description = document.getElementById('description');
+let idx = 0;
+const descriptionText = 'Свою первую строчку кода написал ещё 3 года назад, когда узнал что мой приятель умеет создавать сайты и захотелось самому) Тогда и начался мой путь frontend разработчика на react.';
+
+function typeWriter() {
+  if (idx < descriptionText.length) {
+    description.innerHTML += descriptionText.charAt(idx);
+    idx++;
+    setTimeout(typeWriter, 30);
+  }
+}
+
+let options = {
+    rootMargin: '0px',
+    threshold: 0
+  };
+target = document.querySelector('.about__code');
+
+observerDescription = new IntersectionObserver(entries => {
+var entry = entries[0];
+if (entry.isIntersecting) {
+    typeWriter();
+}
+}, options);
+
+observerDescription.observe(target);
